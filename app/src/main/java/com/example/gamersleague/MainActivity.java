88315f1156d1a11f9a65c7d3d67385268b1ae609
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-19 13:34:18 +0300

    bind views to main avtivity

--- app/src/main/java/com/example/gamersleague/MainActivity.java
+++ app/src/main/java/com/example/gamersleague/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.gamersleague;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.submitUsernameButton)
+    Button mUsernameButton;
+    @BindView(R.id.usernameEditText)
+    EditText mUsernameEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mUsernameButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
