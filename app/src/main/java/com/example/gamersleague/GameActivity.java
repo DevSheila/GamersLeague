707b020da09ba802599e50cbdd6a8fc074b8174e
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-19 13:49:17 +0300

    bind views to single game activity UI

--- app/src/main/java/com/example/gamersleague/GameActivity.java
+++ app/src/main/java/com/example/gamersleague/GameActivity.java
@@ -1,6 +1,55 @@
 package com.example.gamersleague;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class GameActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+
+public class GameActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    @BindView(R.id.gameDetailsTextView)
+    TextView mgameDetailsTextView;
+    @BindView(R.id.reviewslistView)
+    ListView mReviewsListView;
+    @BindView(R.id.addCommentButton)
+    Button maddCommentButton;
+
+
+    private String[] comments = new String[]{"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food"};
+    private String[] ratings = new String[]{"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian"};
+
+    ArrayList<String> allComments = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        ButterKnife.bind(this);
+        allComments.addAll(Arrays.asList(comments));
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }
