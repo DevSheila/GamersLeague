c27fd36c8c4167d250787fda27b68990394e26dc
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-19 13:38:03 +0300

    bind views to games activity and add dummy data

--- app/src/main/java/com/example/gamersleague/GamesActivity.java
+++ app/src/main/java/com/example/gamersleague/GamesActivity.java
@@ -1,6 +1,29 @@
 package com.example.gamersleague;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class GamesActivity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class GamesActivity extends AppCompatActivity {
+    @BindView(R.id.usernameTextView)
+    TextView mUsernameTextView;
+    @BindView(R.id.listView)
+    ListView mListView;
+
+    private String[] games = new String[]{"Sweet Hereafter", "Cricket", "Hawthorne Fish House", "Viking Soul Food", "Red Square", "Horse Brass", "Dick's Kitchen", "Taco Bell", "Me Kha Noodle Bar", "La Bonita Taqueria", "Smokehouse Tavern", "Pembiche", "Kay's Bar", "Gnarly Grey", "Slappy Cakes", "Mi Mero Mole"};
+    private String[] gameDetails = new String[]{"Vegan Food", "Breakfast", "Fishs Dishs", "Scandinavian", "Coffee", "English Food", "Burgers", "Fast Food", "Noodle Soups", "Mexican", "BBQ", "Cuban", "Bar Food", "Sports Bar", "Breakfast", "Mexican"};
+
 }
