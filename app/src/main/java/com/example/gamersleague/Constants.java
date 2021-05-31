1132be74d31abdee823befc0dff50be7eb7c35d2
Sheila Sharon <devsheilawambui@gmail.com>
2021-05-31 04:35:11 +0300

    setup github credentials

---
+++ app/src/main/java/com/example/gamersleague/Constants.java
@@ -0,0 +1,6 @@
+package com.example.gamersleague;
+
+public class Constants {
+    public static  final String GIANT_BOMB_BASE_URL="https://www.giantbomb.com/api/";
+    public static final String GIANT_BOMB_KEY = BuildConfig.GIANT_BOMB_KEY;
+}
