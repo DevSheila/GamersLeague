38bd047adfd58d28e9c20a3d9a49190686e81390
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-02 05:41:43 +0300

    add redirect option from login to sign up

--- app/src/main/java/com/example/gamersleague/ui/LoginActivity.java
+++ app/src/main/java/com/example/gamersleague/ui/LoginActivity.java
@@ -1,16 +1,35 @@
 package com.example.gamersleague.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gamersleague.R;
 
-public class LoginActivity extends  AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends  AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.registerTextView)
+    TextView mRegisterTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
 }
