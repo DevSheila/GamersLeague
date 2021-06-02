fad054fa66a675aed65990d61d59fb5336878c60
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-02 08:13:55 +0300

    bind views to sign up activity

--- app/src/main/java/com/example/gamersleague/ui/SignUpActivity.java
+++ app/src/main/java/com/example/gamersleague/ui/SignUpActivity.java
@@ -1,16 +1,55 @@
 package com.example.gamersleague.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.SingleLineTransformationMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gamersleague.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-public class SignUpActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = SignUpActivity.class.getSimpleName();
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
+    @BindView(R.id.createUserButton)Button mCreateUserButton;
+    @BindView(R.id.nameEditText)EditText mNameEditText;
+    @BindView(R.id.emailEditText) EditText mEmailEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.loginTextView)TextView mLoginTextView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        ButterKnife.bind(this);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateUserButton.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+
+//        createAuthStateListener();
     }
+
+
+
+
+
 }
