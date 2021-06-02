0e2d66a0df4bc6a706d7066f69161600f54e0e78
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-02 05:19:43 +0300

    create sign up activity layout

---
+++ app/src/main/java/com/example/gamersleague/ui/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.gamersleague.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.gamersleague.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
