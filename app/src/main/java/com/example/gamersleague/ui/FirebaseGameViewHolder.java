cccc53fea0c1389675ac047741cad29eb9536daa
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-03 05:21:50 +0300

    create firebase game view holder

---
+++ app/src/main/java/com/example/gamersleague/ui/FirebaseGameViewHolder.java
@@ -0,0 +1,86 @@
+package com.example.gamersleague.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.gamersleague.Constants;
+import com.example.gamersleague.R;
+import com.example.gamersleague.models.Result;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+
+public class FirebaseGameViewHolder  extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    View mView;
+    Context mContext;
+
+    public FirebaseGameViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+    public void bindGame(Result mResult) {
+        ImageView gameImageView = (ImageView) mView.findViewById(R.id.gameImageView);
+        TextView nameTextView = (TextView) mView.findViewById(R.id.gameNameTextView);
+        TextView descriptionTextView = (TextView) mView.findViewById(R.id.descriptionTextView);
+        TextView ratingTextView = (TextView) mView.findViewById(R.id.ratingTextView);
+
+        Picasso.get().load(mResult.getImage().getScreenUrl()).into(gameImageView);
+        nameTextView.setText(mResult.getName());
+        descriptionTextView.setText(mResult.getDeck());
+        ratingTextView.setText("Rating: " + mResult.getOriginalGameRating() + "/5");
+    }
+
+    @Override
+    public void onClick(View v) {
+        final ArrayList<Result> games = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_GAMES);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                   games.add(snapshot.getValue(Result.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, GamesDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("restaurants", Parcels.wrap(games));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+}
+}
+
+
+
+
+
+
+
+
