92b0a2472a2552295bdb7d843ad59f77c774ea90
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-09 23:08:40 +0300

    remove commented out code

--- app/src/main/java/com/example/gamersleague/network/GiantBombClient.java
+++ app/src/main/java/com/example/gamersleague/network/GiantBombClient.java
@@ -21,15 +21,6 @@ public class GiantBombClient {
 
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
             .addInterceptor(loggingInterceptor)
-//            .addInterceptor(new Interceptor() {
-//                @Override
-//                public Response intercept(Chain chain) throws IOException {
-//                    Request newRequest  = chain.request().newBuilder()
-//                            .addHeader("Authorization",GIANT_BOMB_KEY)
-//                            .build();
-//                    return chain.proceed(newRequest);
-//                }
-//        })
                     .build();
         Gson gson = new GsonBuilder()
                 .serializeNulls()
@@ -45,43 +36,5 @@ public class GiantBombClient {
 
 
         return retrofit.create(GiantBombApi.class);
-
-        // Define the interceptor, add authentication headers
-//        Interceptor interceptor = new Interceptor() {
-//            @Override
-//            public okhttp3.Response intercept(Chain chain) throws IOException {
-//                Request newRequest = chain.request()
-//                        .newBuilder()
-//                        .addHeader("Authorization", GIANT_BOMB_KEY)
-//                        .build();
-//                return chain.proceed(newRequest);
-//            }
-//        };
-//        // Add the interceptor to OkHttpClient
-//        OkHttpClient.Builder builder = new OkHttpClient.Builder();
-//        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
-//
-//// Can be Level.BASIC, Level.HEADERS, or Level.BODY
-//
-//        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-//        builder.networkInterceptors().add(httpLoggingInterceptor);
-////        builder.build();
-//        builder.interceptors().add(interceptor);
-//        OkHttpClient client = builder.build();
-//
-//                Gson gson = new GsonBuilder()
-//                .setLenient()
-//                .create();
-//
-//        // Add the interceptor to OkHttpClient
-//
-//        // Set the custom client when building adapter
-//        Retrofit retrofit = new Retrofit.Builder()
-//                .baseUrl(GIANT_BOMB_BASE_URL)
-//                .addConverterFactory(GsonConverterFactory.create(gson))
-//                .client(client)
-//                .build();
-//
-//        return retrofit.create(GiantBombApi.class);
     }
 }
