fa871920e172f31d6d0e10f4593d11ea7f6a791b
Sheila Sharon <devsheilawambui@gmail.com>
2021-06-09 19:39:10 +0300

    add animation

--- app/src/main/java/com/example/gamersleague/ui/MainActivity.java
+++ app/src/main/java/com/example/gamersleague/ui/MainActivity.java
@@ -4,55 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 
 import com.example.gamersleague.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.submitUsernameButton)
-    Button mUsernameButton;
-    @BindView(R.id.usernameEditText)
-    EditText mUsernameEditText;
+    @BindView(R.id.appTitle)TextView mAppTitle;
+    @BindView(R.id.appTitle2)TextView mAppTitle2;
+
+
+    //animation variables
+    Animation topAnim,bottomAnim;
+    private static int SPLASH_SCREEN=5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-
         ButterKnife.bind(this);
-        mUsernameButton.setOnClickListener(this);
-    }
 
-    @Override
-    public void onClick(View v) {
 
-        if(v == mUsernameButton){
-            String username = mUsernameEditText.getText().toString();
+        //define animation context
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
 
-            if(username.isEmpty()){
-                Log.i("username",username);
-                Toast.makeText(MainActivity.this, "Please Enter Username", Toast.LENGTH_LONG).show();
-                mUsernameEditText.setError("Please Enter Your Username");
-            }else{
-                Log.i("username",username);
-                Intent intent = new Intent(MainActivity.this, GamesActivity.class);
-                intent.putExtra("username", username);
-                mUsernameEditText.setText(" ");
+        //hooks
 
+        mAppTitle.setAnimation(topAnim);
+
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
-
+                finish();//remove activity from activity stack
 
             }
-
-        }
+        }, SPLASH_SCREEN);
 
 
     }
